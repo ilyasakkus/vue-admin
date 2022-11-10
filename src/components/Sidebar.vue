1a083afa9dc886d12ac1b63b6665ed6de3e1419f
İlyas Akkuş <ilyasbote@gmail.com>
2022-11-10 12:00:05 +0300

    minor updates

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -102,7 +102,7 @@
         </router-link>
       </div>
       <div class="item mt-3">
-        <router-link to="/">
+        <router-link to="/component/ilanlar">
           <button
             class="text-gray-800 dark:text-gray-500 bg-transparent hover:bg-gray-100 dark:hover:bg-gray-700 w-full flex text-left rounded-md box-border p-3"
           >
@@ -112,6 +112,7 @@
           </button>
         </router-link>
       </div>
+      
     </div>
   </nav>
 </template>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -891,12 +891,12 @@ Giriş Yap              </h1>
               <div class="col-span-6 sm:col-span-3">
                 <label for="country" class="block text-sm font-medium text-gray-700">İş Pozisyonu</label>
                 <select id="country" name="country" autocomplete="country-name" class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                  <option>Türkiye</option>
-                  <option>Azerbaycan</option>
-                  <option>Türkmenistan</option>
-                  <option>Suriye</option>
-                  <option>İran</option>
-                  <option>Irak</option>
+                  <option>Tekniker</option>
+                  <option>Memur</option>
+                  <option>Akademisyen</option>
+                  <option>Öğretmen</option>
+                  <option>Mühendis</option>
+                  <option>Yönetici</option>
                 </select>
               </div>
               
